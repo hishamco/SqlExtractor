3e777a93161f8a68f2aa6d3ad282ae94daf8ae14
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-11 22:52:04 +0300

    Add RazorProject

---
+++ src/SqlExtractor.Razor/RazorProject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using SqlExtractor.Core;
+
+namespace SqlExtractor.Razor
+{
+    public class RazorProject : IProject
+    {
+        public const string RazorFileExtension = ".cshtml";
+
+        public RazorProject(string path)
+        {
+            Path = path;
+            LoadFiles();
+        }
+
+        public string Path { get; }
+
+        public string Extension => ".csproj";
+
+        public IEnumerable<string> Files { get; private set; }
+
+        private void LoadFiles()
+        {
+            var projectDirectoryPath = new FileInfo(Path).Directory.FullName;
+            var razorFilesPattern = "*" + RazorFileExtension;
+            Files = Directory.EnumerateFiles(projectDirectoryPath, razorFilesPattern, SearchOption.AllDirectories);
+        }
+    }
+}
