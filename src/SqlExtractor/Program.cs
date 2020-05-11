40ad6e307c9d97ce0505379dc90b005ac8c5dc7b
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-11 20:18:10 +0300

    Add project skeleton

---
+++ src/SqlExtractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SQL Extractor!");
+        }
+    }
+}
