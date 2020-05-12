9af0bc3c67edcc853f86a8aab66ebbb2db20e316
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-12 07:41:18 +0300

    Add SqlWriter to persist the generated SQL

---
+++ src/SqlExtractor.Core/Sql/SqlWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SqlExtractor.Core.Sql
+{
+    public class SqlWriter
+    {
+        public const string SqlScriptFileExtension = ".sql";
+
+        private readonly string _path;
+
+        public SqlWriter(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException("The path can't be null or empty.", nameof(path));
+            }
+
+            _path = path;
+        }
+
+        public async Task WriteLineAsync(string contents) => await File.WriteAllTextAsync(_path, contents);
+
+        public async Task WriteLineAsync(IEnumerable<string> contents) => await File.WriteAllLinesAsync(_path, contents);
+    }
+}
