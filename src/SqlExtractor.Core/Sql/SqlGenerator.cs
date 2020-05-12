0f06b0703dacaf6ace435fe19d2a184ccfaca0ad
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-12 16:17:54 +0300

    Add missing value in SqlGenerator

--- src/SqlExtractor.Core/Sql/SqlGenerator.cs
+++ src/SqlExtractor.Core/Sql/SqlGenerator.cs
@@ -3,6 +3,6 @@
     public class SqlGenerator : ISqlGenerator
     {
         public string Generate(LocalizedString localizedString)
-            => $"INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES ('en-US', N'{localizedString.Text}');";
+            => $"INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES ('en-US', N'{localizedString.Text}', N'{localizedString.Text}');";
     }
 }
